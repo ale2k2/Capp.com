ce222fb2f621bfce23e81d26ed8e273d9dee2e4f
ale2k2 <80537311+ale2k2@users.noreply.github.com>
2023-02-19 14:38:48 -0500

    Update app.js

--- capstone-project/app.js
+++ capstone-project/app.js
@@ -5,7 +5,13 @@
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "https://www.carboninterface.com/api/v1/auth");
 xhr.setRequestHeader("Authorization", "Bearer xS7W8Yyg5tIqwKaQtCnurQ");
-xhr.send();
+xhr.send(JSON.stringify({
+    "type": "electricity",
+        "electricity_unit": "mwh",
+        "electricity_value": 42,
+        "country": "us",
+        "state": "fl"
+}));
 xhr.responseType = "json";
 xhr.onload = () => {
   if (xhr.readyState == 4 && xhr.status == 200) {
