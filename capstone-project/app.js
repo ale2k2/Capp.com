7772951c8252108d779ee56d9a44562b0c958b62
ale2k2 <80537311+ale2k2@users.noreply.github.com>
2023-02-19 11:03:55 -0500

    Update app.js

--- capstone-project/app.js
+++ capstone-project/app.js
@@ -1,3 +1,17 @@
 'use strict';
 
-search = document.querySelector('#search');
+//search = document.querySelector('#search');
+
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.carboninterface.com/api/v1/auth");
+xhr.setRequestHeader("Authorization", "Bearer xS7W8Yyg5tIqwKaQtCnurQ");
+xhr.send();
+xhr.responseType = "json";
+xhr.onload = () => {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    const data = xhr.response;
+    console.log(data);
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+};
