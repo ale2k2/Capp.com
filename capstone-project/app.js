538e921dbc64af15e90841f2228ebdd7fce66939
ale2k2 <80537311+ale2k2@users.noreply.github.com>
2023-02-20 14:08:12 -0500

    Update app.js

--- capstone-project/app.js
+++ capstone-project/app.js
@@ -7,15 +7,14 @@ xhr.open("POST", "https://www.carboninterface.com/api/v1/estimates");
 xhr.setRequestHeader("Authorization", "Bearer xS7W8Yyg5tIqwKaQtCnurQ");
 xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send(JSON.stringify({
-    "type": "electricity",
-        "electricity_unit": "mwh",
-        "electricity_value": 42,
-        "country": "us",
-        "state": "fl"
+  "type": "vehicle",
+  "distance_unit": "mi",
+  "distance_value": 100,
+  "vehicle_model_id": "7268a9b7-17e8-4c8d-acca-57059252afe9"
 }));
 xhr.responseType = "json";
 xhr.onload = () => {
-  if (xhr.readyState == 4 && xhr.status == 200) {
+  if (xhr.readyState == 4) {
     const data = xhr.response;
     console.log(data);
   } else {
