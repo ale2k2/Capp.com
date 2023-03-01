1c4a9c60ad73c796f4193faa3080528343a2abb3
ale2k2 <80537311+ale2k2@users.noreply.github.com>
2023-03-01 12:57:56 -0500

    Update app.js

--- capstone-project/app.js
+++ capstone-project/app.js
@@ -1,23 +1,15 @@
 'use strict';
 
-//search = document.querySelector('#search');
-
-const xhr = new XMLHttpRequest();
-xhr.open("POST", "https://www.carboninterface.com/api/v1/estimates");
-xhr.setRequestHeader("Authorization", "Bearer xS7W8Yyg5tIqwKaQtCnurQ");
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.send(JSON.stringify({
-  "type": "vehicle",
-  "distance_unit": "mi",
-  "distance_value": 100,
-  "vehicle_model_id": "7268a9b7-17e8-4c8d-acca-57059252afe9"
-}));
-xhr.responseType = "json";
-xhr.onload = () => {
-  if (xhr.readyState == 4) {
-    const data = xhr.response;
-    console.log(data);
-  } else {
-    console.log(`Error: ${xhr.status}`);
-  }
-};
+const url = "https://www.carboninterface.com/api/v1/estimates"
+const data = {"type": "vehicle", "distance_unit": "mi", "distance_value": 100, "vehicle_model_id": "7268a9b7-17e8-4c8d-acca-57059252afe9"}
+fetch(url, {
+  method: "POST",
+  headers: {
+    "Authorization": "Bearer xS7W8Yyg5tIqwKaQtCnurQ",
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(data)
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error(error));
