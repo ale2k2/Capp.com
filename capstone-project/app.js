a1cef53c9063e10fe36a2f596ccc21f507d94544
ale2k2 <80537311+ale2k2@users.noreply.github.com>
2023-04-20 18:52:23 -0400

    Update app.js

--- capstone-project/app.js
+++ capstone-project/app.js
@@ -1,21 +1,10 @@
-'use strict'
+'use strict';
 
-/*const url = "https://www.carboninterface.com/api/v1/estimates"
-const data = {"type": "vehicle", "distance_unit": "mi", "distance_value": 100, "vehicle_model_id": "7268a9b7-17e8-4c8d-acca-57059252afe9"}
-fetch(url, {
-  method: "POST",
-  headers: {
-    "Authorization": "Bearer xS7W8Yyg5tIqwKaQtCnurQ",
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(data) 
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error));*/
+let total = 0;
+const url = "https://www.carboninterface.com/api/v1/estimates";
 
-var total = 0;
 
+// Electricity estimate
 const form = document.querySelector('form');
 const result = document.querySelector('#result');
 form.addEventListener('submit', function(event) {
@@ -28,13 +17,30 @@ form.addEventListener('submit', function(event) {
     total += totalEmissions;
     result6.innerText = total.toFixed(2);
     result.innerText = totalEmissions.toFixed(2);
+    let apiDat = 0;
+
+    /*const data = {"type": "electricity", "electricity_unit": "mwh", "electricity_value": electricity, "country": "us", "state": "fl"};
+    fetch(url, {
+    method: "POST",
+    headers: {
+        "Authorization": "Bearer xS7W8Yyg5tIqwKaQtCnurQ",
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data) 
+    })
+    .then(response => response.json())
+    .then(data => apiData = data.data.attributes.carbon_mt)
+    .catch(error => console.error(error));*/
+
     document.querySelector('#electricity').disabled = true;
     document.querySelector('#natural-gas').disabled = true;
     document.querySelector('#vehicle-miles').disabled = true;
     document.querySelector('#air-travel').disabled = true;
     document.querySelector('#buttonOne').disabled = true;
+
 });
 
+// Flight estimate
 const form2 = document.querySelector('#form2');
 const result2 = document.querySelector('#result2');
 form2.addEventListener('submit', function(event) {
@@ -47,6 +53,22 @@ form2.addEventListener('submit', function(event) {
     total += totalEmissions2;
     result6.innerText = total.toFixed(2);
     result2.innerText = totalEmissions2.toFixed(2);
+    let apiData = 0;
+
+    /*const data = {"type": "flight", "passengers": 2, "legs": [{"departure_airport": "sfo", "destination_airport": "yyz"}, {"departure_airport": "yyz", "destination_airport": "sfo"}]};
+    fetch(url, {
+    method: "POST",
+    headers: {
+        "Authorization": "Bearer xS7W8Yyg5tIqwKaQtCnurQ",
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data) 
+    })
+    .then(response => response.json())
+    .then(data => apiData = data.data.attributes.carbon_mt)
+    .catch(error => console.error(error));*/
+
+
     document.querySelector('#electricity2').disabled = true;
     document.querySelector('#natural-gas2').disabled = true;
     document.querySelector('#vehicle-miles2').disabled = true;
@@ -54,6 +76,7 @@ form2.addEventListener('submit', function(event) {
     document.querySelector('#buttonTwo').disabled = true;
 });
 
+// Shipping estimate
 const form3 = document.querySelector('#form3');
 const result3 = document.querySelector('#result3');
 form3.addEventListener('submit', function(event) {
@@ -66,6 +89,21 @@ form3.addEventListener('submit', function(event) {
     total += totalEmissions3;
     result6.innerText = total.toFixed(2);
     result3.innerText = totalEmissions3.toFixed(2);
+    let apiDat = 0;
+
+    /*const data = {"type": "shipping", "weight_unit": "g", "weight_value": 200, "distance_unit": "mi", "distance_value": 2000, "transport_method": "truck"};
+    fetch(url, {
+    method: "POST",
+    headers: {
+        "Authorization": "Bearer xS7W8Yyg5tIqwKaQtCnurQ",
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data) 
+    })
+    .then(response => response.json())
+    .then(data => apiData = data.data.attributes.carbon_mt)
+    .catch(error => console.error(error));*/
+
     document.querySelector('#electricity3').disabled = true;
     document.querySelector('#natural-gas3').disabled = true;
     document.querySelector('#vehicle-miles3').disabled = true;
@@ -73,6 +111,8 @@ form3.addEventListener('submit', function(event) {
     document.querySelector('#buttonThree').disabled = true;
 });
 
+
+// Vehicle estimate
 const form4 = document.querySelector('#form4');
 const result4 = document.querySelector('#result4');
 form4.addEventListener('submit', function(event) {
@@ -85,6 +125,21 @@ form4.addEventListener('submit', function(event) {
     total += totalEmissions4;
     result6.innerText = total.toFixed(2);
     result4.innerText = totalEmissions4.toFixed(2);
+    let apiDat = 0;
+
+    /*const data = {"type": "vehicle", "distance_unit": "mi", "distance_value": 100, "vehicle_model_id": "7268a9b7-17e8-4c8d-acca-57059252afe9"};
+    fetch(url, {
+    method: "POST",
+    headers: {
+        "Authorization": "Bearer xS7W8Yyg5tIqwKaQtCnurQ",
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data) 
+    })
+    .then(response => response.json())
+    .then(data => apiData = data.data.attributes.carbon_mt)
+    .catch(error => console.error(error));*/
+
     document.querySelector('#electricity4').disabled = true;
     document.querySelector('#natural-gas4').disabled = true;
     document.querySelector('#vehicle-miles4').disabled = true;
@@ -92,6 +147,7 @@ form4.addEventListener('submit', function(event) {
     document.querySelector('#buttonFour').disabled = true;
 });
 
+// Fuel estimate
 const form5 = document.querySelector('#form5');
 const result5 = document.querySelector('#result5');
 form5.addEventListener('submit', function(event) {
@@ -104,6 +160,21 @@ form5.addEventListener('submit', function(event) {
     total += totalEmissions5;
     result6.innerText = total.toFixed(2);
     result5.innerText = totalEmissions5.toFixed(2);
+    let apiDat = 0;
+
+    /*const data = {"type": "fuel_combustion", "fuel_source_type": "dfo", "fuel_source_unit": "btu", "fuel_source_value": 2};
+    fetch(url, {
+    method: "POST",
+    headers: {
+        "Authorization": "Bearer xS7W8Yyg5tIqwKaQtCnurQ",
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data) 
+    })
+    .then(response => response.json())
+    .then(data => apiData = data.data.attributes.carbon_mt)
+    .catch(error => console.error(error));*/
+
     document.querySelector('#electricity5').disabled = true;
     document.querySelector('#natural-gas5').disabled = true;
     document.querySelector('#vehicle-miles5').disabled = true;
